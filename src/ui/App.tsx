75a9eba145593e2bdf5d60c1c2ba32da8aa611a3
Mateus PS <mateus.castro2011@gmail.com>
2025-10-26 15:24:46 -0300

    feat: add protected route for Settings page

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,6 +3,7 @@ import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { DashboardProvider } from './contexts/DashboardContext';
 import Dashboard from './pages/Dashboard';
 import Developers from './pages/Developers';
+import Settings from './pages/Settings';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedRoute from './components/auth/ProtectedRoute';
@@ -38,6 +39,14 @@ function AppRoutes() {
         element={
           <ProtectedRoute>
             <Developers />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <Settings />
           </ProtectedRoute>
         }
       />
